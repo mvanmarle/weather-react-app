d8ccf87829750076ed687509d9cebda0d7b64ec5
mvanmarle <97441731+mvanmarle@users.noreply.github.com>
2022-07-13 08:46:26 +0200

    Debugged app

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default function App() {
             <a
               href="https://github.com/mvanmarle/weather-react-app"
               target="_blank"
+              rel="noreferrer"
             >
               Open source code
             </a>
